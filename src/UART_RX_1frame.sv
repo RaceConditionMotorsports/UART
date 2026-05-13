0a9cedb1eab383e429acdc793101614870ebb20f
peterg <petergdev7@gmail.com>
2026-05-13 19:52:31 +0100

    add reset; change naming convention; change clock API (?)

--- src/UART_RX_1frame.sv
+++ src/UART_RX_1frame.sv
@@ -21,14 +21,15 @@
 
 
 module UART_RX_1frame(
-    input RX,   //(ToDo) serial data input
-    input BCLK, //(ToDo) receives clock, preferably 115200
-    input EN,   //(ToDo) 0 => module disabled, 1 => module all healthy
+    input clock, //(ToDo) receives clock, preferably 16x115200 = 18.432MHz
+    input reset_n,  //(ToDo) receives clock 
+    input rx,   //(ToDo) serial data input
+    input en,   //(ToDo) 0 => module disabled, 1 => module all healthy
     input parity_yes, //(ToDo) 0 => no parity bit, 1 => yes parity bit
     input stop_2b, //(ToDo) 0 => 1 stop bit, 1 => 2 stop bits
-    output [7:0] FRAME_lower, //(ToDo) displays data from most recently received valid frame
-    output NEW_FRAME, //(ToDo) switches to 1 and then to 0 when new data is on FRAME_lower
-    output ERROR      //(ToDo) switches to 1 after invalid frame is receiver until a new start byte
+    output logic [7:0] frame_out, //(ToDo) displays data from most recently received valid frame
+    output logic new_frame, //(ToDo) switches to 1 and then to 0 when new data is on FRAME_lower
+    output logic error      //(ToDo) switches to 1 after invalid frame is receiver until a new start byte
     );
     
 //put your logic here
