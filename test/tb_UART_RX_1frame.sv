46ea73b0de4d8e4413061dbed3481d3bfc29f5b2
peterg <petergdev7@gmail.com>
2026-05-13 20:47:20 +0100

    feat: add testbench for reset

---
+++ test/tb_UART_RX_1frame.sv
@@ -0,0 +1,59 @@
+`timescale 1ns / 100ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/07/2026 09:27:33 PM
+// Design Name: 
+// Module Name: tb_UART_RX_1frame
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_UART_RX_1frame();
+
+logic clock, reset_n, rx, enable, parity_yes, stop_2b, new_frame, error;
+logic [7:0] frame;
+
+/**
+* Device under test - UART_RX_1frame
+*/
+uart_rx_1frame dut(
+    .reset_n (reset_n),
+    .clock (clock),
+    .rx (RX),
+    .en (enable),
+    .parity_yes (parity_yes),
+    .stop_2b (stop_2b),
+    .new_frame (new_frame),
+    .error (error)
+);
+
+//initialisation
+initial begin
+   clock <= 0;
+   reset_n <= 1;
+end
+    
+//simulation
+always begin
+    #5
+    clock = ~clock;
+end
+
+always begin
+    #200 
+    reset_n = ~reset_n;
+end
+
+endmodule
